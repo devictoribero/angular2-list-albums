865e8ad5c6032f04b074af629cd286fcbe3a29fd
Victor Ribero <victor.ribero3@gmail.com>
2018-04-29 17:37:33 +0200

    Changed the view of album detail

--- src/app/album/details/component/album-details.component.ts
+++ src/app/album/details/component/album-details.component.ts
@@ -1,16 +1,30 @@
-import { Component, OnInit} from '@angular/core';
-import {MOCK_ALBUM_DETAILS} from '../../../../mocks/album';
+import {Component, OnInit} from '@angular/core';
+import Album from '../../entity/Album';
+import {GetAlbumsByArtistId} from '../../list/services/GetAlbumsByArtistId';
+import Artist from '../../../Artist/entity/Artist';
+import Track from '../../../track/entity/Track';
+import {GetTracksFromAlbumById} from '../services/GetTracksFromAlbumById';
 
 @Component({
-    selector: 'app-album-details',
-    templateUrl: './album-details.component.html',
-    styleUrls: ['./album-details.component.css'],
+  selector: 'app-album-details',
+  templateUrl: './album-details.component.html',
+  styleUrls: ['./album-details.component.css'],
 })
 
 export class AlbumDetailsComponent implements OnInit {
-    album: object = MOCK_ALBUM_DETAILS;
+  artist: Artist;
+  tracks: Array<Track> = [];
+  getTracksFromAlbumById: GetTracksFromAlbumById;
 
-    constructor() { }
+  constructor(service: GetTracksFromAlbumById) {
+    this.getTracksFromAlbumById = service;
+  }
 
-    ngOnInit() {}
+  ngOnInit() {
+    const albumId = 909253;
+    this.getTracksFromAlbumById.handle(albumId).then(result => {
+      this.tracks = result.albums;
+      this.artist = result.artist;
+    });
+  }
 }
